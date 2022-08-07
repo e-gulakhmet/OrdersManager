60c7aaea554534e174d09a5064466be96e68494d
who-man-tech <enes515254@gmail.com>
2022-08-07 11:40:07 +0200

    Added field price in rub to Order Model

--- server/app/orders/models.py
+++ server/app/orders/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Order(models.Model):
+    number = models.IntegerField('Order number')
+    price_in_usd = models.IntegerField('Price in USD')
+    price_in_rub = models.IntegerField('Price in RUB')
+
+    delivery_date = models.DateField()
+
+    def __str__(self):
+        return f'Order {self.number}, {self.price}₽, {self.delivery_date}'
